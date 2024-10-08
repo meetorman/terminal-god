42ee595d485ef21c21c7082129fcfcf92a02c210
meetorman <134105842+meetorman@users.noreply.github.com>
2024-10-07 19:19:28 -0700

    fudge

--- install_luarocks.sh
+++ install_luarocks.sh
@@ -12,6 +12,7 @@ mkdir -p "$USER_HOME/.local/lib"
 mkdir -p "$USER_HOME/.local/share/lua/5.1"
 mkdir -p "$USER_HOME/.local/lib/lua/5.1"
 mkdir -p "$USER_HOME/.luarocks"
+mkdir -p "$USER_HOME/.local/etc/luarocks"
 
 # Download and install Lua
 cd /tmp
@@ -21,11 +22,11 @@ cd lua-5.1.5
 make macosx INSTALL_TOP="$USER_HOME/.local"
 make install INSTALL_TOP="$USER_HOME/.local"
 
-# Download and install LuaRocks
+# Download and install LuaRocks (version 3.9.2)
 cd /tmp
-curl -R -O https://luarocks.github.io/luarocks/releases/luarocks-3.11.1.tar.gz
-tar zxf luarocks-3.11.1.tar.gz
-cd luarocks-3.11.1
+curl -R -O https://luarocks.github.io/luarocks/releases/luarocks-3.9.2.tar.gz
+tar zxf luarocks-3.9.2.tar.gz
+cd luarocks-3.9.2
 ./configure --prefix="$USER_HOME/.local" \
             --with-lua="$USER_HOME/.local" \
             --sysconfdir="$USER_HOME/.local/etc/luarocks" \
@@ -33,16 +34,8 @@ cd luarocks-3.11.1
 make build
 make install
 
-# Configure LuaRocks
-$USER_HOME/.local/bin/luarocks config --local lua_dir $USER_HOME/.local
-$USER_HOME/.local/bin/luarocks config --local lua_interpreter $USER_HOME/.local/bin/lua
-
-# Set up LuaRocks environment
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$USER_HOME/.zshrc"
-echo 'eval $($HOME/.local/bin/luarocks path --bin)' >> "$USER_HOME/.zshrc"
-
 # Create custom LuaRocks configuration
-cat > "$USER_HOME/.luarocks/config-5.1.lua" << EOL
+cat > "$USER_HOME/.local/etc/luarocks/config-5.1.lua" << EOL
 rocks_trees = {
   { name = "user", root = "$USER_HOME/.local" }
 }
@@ -54,5 +47,11 @@ variables = {
   LUA_BINDIR = "$USER_HOME/.local/bin",
 }
 EOL
+
+# Set up LuaRocks environment
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$USER_HOME/.zshrc"
+echo 'export LUA_PATH="$HOME/.local/share/lua/5.1/?.lua;$HOME/.local/share/lua/5.1/?/init.lua;;"' >> "$USER_HOME/.zshrc"
+echo 'export LUA_CPATH="$HOME/.local/lib/lua/5.1/?.so;;"' >> "$USER_HOME/.zshrc"
+echo 'export LUAROCKS_CONFIG="$HOME/.local/etc/luarocks/config-5.1.lua"' >> "$USER_HOME/.zshrc"
 
 echo "Installation complete. Please run 'source ~/.zshrc' to update your current shell session."
