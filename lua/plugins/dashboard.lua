68b084c0c42b7a5c76ffec86afcc3c6df48d62be
meetorman <134105842+meetorman@users.noreply.github.com>
2024-10-08 20:56:31 -0700

    Dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,5 +1,45 @@
 return {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    dependencies = { {'nvim-tree/nvim-web-devicons'}}
+	'nvimdev/dashboard-nvim',
+	event = 'VimEnter',
+	config = function()
+		local function footer()
+			local stats = require('lazy').stats()
+			local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+			return { 
+				'',
+				'⚡ Terminal god loaded in ' .. ms .. 'ms' 
+			}
+		end
+
+		require('dashboard').setup {
+			theme = 'hyper',
+			config = {
+				week_header = {
+					enable = true,
+				},
+				shortcut = {
+						{ desc = ' Find File', group = 'DashboardShortCut', action = 'Telescope find_files', key = 'f' },
+						{ desc = ' New File', group = 'DashboardShortCut', action = 'enew', key = 'n' },
+						{ desc = ' Recent Files', group = 'DashboardShortCut', action = 'Telescope oldfiles', key = 'r' },
+						{ desc = ' Settings', group = 'DashboardShortCut', action = 'edit $MYVIMRC', key = 's' },
+				},
+				packages = { enable = false },  -- Disable the package info
+				project = { 
+					enable = false, 
+					limit = 8, 
+					icon = '󰏓 ', 
+					label = 'Recent Projects', 
+					action = 'Telescope find_files cwd=' 
+				},
+				mru = { 
+					limit = 10, 
+					icon = '󰋚 ', 
+					label = 'Recent Files', 
+					cwd_only = false 
+				},
+				footer = footer,  -- Use the custom footer function
+			}
+		}
+	end,
+	dependencies = { {'nvim-tree/nvim-web-devicons'}}
 }
